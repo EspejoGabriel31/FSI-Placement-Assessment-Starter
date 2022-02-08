49e0c5d430d6a160b00ebe1b44f2fc069d866088
unknown <gespejo2144@gmail.com>
2022-02-07 18:42:37 -0800

    completed assessment

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Gabriel Espejo" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -18,10 +18,63 @@ credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    // HINT: You can delete this console.log after you no longer need it!
+    //console.log('Gingerbread + button was clicked!')
+    
+    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    gbCount.textContent = ++gb
+    updateTotal()
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
+const gbCount = document.querySelector("#qty-gb")
+const gbMinusBtn = document.querySelector('#minus-gb')
+
+const ccCount = document.querySelector('#qty-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const ccPlusBtn = document.querySelector('#add-cc')
+
+const sugarCount = document.querySelector('#qty-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+
+gbMinusBtn.addEventListener('click',function(){
+    if(gbCount.textContent > 0){
+        gbCount.textContent = --gb
+        updateTotal()
+    }
+    
+})
+
+ccMinusBtn.addEventListener('click',function(){
+    if(ccCount.textContent > 0){
+        ccCount.textContent = --cc
+        updateTotal()
+    }
+
+})
+
+ccPlusBtn.addEventListener('click',function(){
+    ccCount.textContent = ++cc 
+    updateTotal()
+})
+
+sugarMinusBtn.addEventListener('click',function(){
+    if(sugarCount.textContent > 0){
+        sugarCount.textContent = --sugar
+        updateTotal()
+    }
+})
+
+sugarPlusBtn.addEventListener('click',function(){
+    sugarCount.textContent = ++sugar
+    updateTotal()
+})
+
+const totalCount = document.querySelector('#qty-total')
+let total = totalCount.textContent
+
+function updateTotal(){
+    total = gb + cc + sugar
+    totalCount.textContent = total
+}
