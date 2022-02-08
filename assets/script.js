d7a89177c91c542e7a4dedefe0c98742099ec54a
unknown <gespejo2144@gmail.com>
2022-02-07 18:56:45 -0800

    added comments

--- assets/script.js
+++ assets/script.js
@@ -27,17 +27,14 @@ gbPlusBtn.addEventListener('click', function() {
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
+
+// selecting the element with an id of qty-gb
 const gbCount = document.querySelector("#qty-gb")
+
+// selecting the element with an id of minus-gb
 const gbMinusBtn = document.querySelector('#minus-gb')
 
-const ccCount = document.querySelector('#qty-cc')
-const ccMinusBtn = document.querySelector('#minus-cc')
-const ccPlusBtn = document.querySelector('#add-cc')
-
-const sugarCount = document.querySelector('#qty-sugar')
-const sugarMinusBtn = document.querySelector('#minus-sugar')
-const sugarPlusBtn = document.querySelector('#add-sugar')
-
+// Event listener for clicks on the "-" button for Gingerbread cookies
 gbMinusBtn.addEventListener('click',function(){
     if(gbCount.textContent > 0){
         gbCount.textContent = --gb
@@ -46,6 +43,16 @@ gbMinusBtn.addEventListener('click',function(){
     
 })
 
+// selecting the element with an id of qty-cc
+const ccCount = document.querySelector('#qty-cc')
+
+// selecting the element with an id of minus-cc
+const ccMinusBtn = document.querySelector('#minus-cc')
+
+// selecting the element with an id of add-cc
+const ccPlusBtn = document.querySelector('#add-cc')
+
+// Event listener for clicks on the "-" button for Chocolatechip cookies
 ccMinusBtn.addEventListener('click',function(){
     if(ccCount.textContent > 0){
         ccCount.textContent = --cc
@@ -54,11 +61,22 @@ ccMinusBtn.addEventListener('click',function(){
 
 })
 
+// Event listener for clicks on the "+" button for Chocolatechip cookies
 ccPlusBtn.addEventListener('click',function(){
     ccCount.textContent = ++cc 
     updateTotal()
 })
 
+// selecting the element with an id of qty-sugar
+const sugarCount = document.querySelector('#qty-sugar')
+
+// selecting the element with an id of minus-sugar
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+
+// selecting the element with an id of add-sugar
+const sugarPlusBtn = document.querySelector('#add-sugar')
+
+// Event listener for clicks on the "-" button for Sugar cookies
 sugarMinusBtn.addEventListener('click',function(){
     if(sugarCount.textContent > 0){
         sugarCount.textContent = --sugar
@@ -66,14 +84,18 @@ sugarMinusBtn.addEventListener('click',function(){
     }
 })
 
+// Event listener for clicks on the "+" button for Sugar cookies
 sugarPlusBtn.addEventListener('click',function(){
     sugarCount.textContent = ++sugar
     updateTotal()
 })
 
+// selecting the element with an id of qty-total
 const totalCount = document.querySelector('#qty-total')
-let total = totalCount.textContent
 
+let total = 0 // Total
+
+// Method that updates Total 
 function updateTotal(){
     total = gb + cc + sugar
     totalCount.textContent = total
